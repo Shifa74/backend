0237770225ea911486cacdaa996c4f9b1cbd85a0
Shifa74 <shifaimran995@gmail.com>
2024-12-31 20:38:25 +0500

    Fix user ID handling in watch history query and update account route methods to use PATCH

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -38,16 +38,16 @@ router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
-router.route("/update-account").get(verifyJWT, updateAccountDetails);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
 router
-  .route("/avatar")
+  .route("/update-avatar")
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
-  .route("cover-image")
-  .patch(verifyJWT, upload.fields("coverImage"), updateCoverImage);
+  .route("/update-coverImg")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
 
-router.route("'/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
